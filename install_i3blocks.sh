6aa88adbd67b35cded250f23d5edc6cb6582938b
John M. Daly <jmdaly@gmail.com>
2015-12-01 14:15:21 -0500

    Added install script for i3blocks

---
+++ install_i3blocks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script downloads, builds, and install i3blocks
+
+# install the required build dependencies:
+apt-get update
+apt-get install -y ruby-ronn
+
+# Get the source:
+if [ ! -f /usr/local/bin/i3blocks ]; then
+  mkdir -p ~/git
+  cd ~/git
+  if [ ! -d ~/git/i3blocks ]; then
+    git clone https://github.com/vivien/i3blocks.git
+  fi
+fi
+
+cd ~/git/i3blocks
+make install

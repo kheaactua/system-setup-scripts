7904fbfd0b7a808cd6856c49247aa914d8d55304
John M. Daly <jmdaly@gmail.com>
2016-01-14 10:29:25 -0500

    Added script to install a newer version of tmux

---
+++ install_tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script sets up a PPA to install the latest
+# version of tmux. The one that ships with Ubuntu
+# 14.04 is too old to use plugins with.
+
+if [ ! -f /etc/apt/sources.list.d/pi-rho-dev-trusty.list ]; then
+  add-apt-repository -y ppa:pi-rho/dev
+fi
+
+apt-get update
+apt-get install -y tmux

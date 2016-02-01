d883502d0b88353a357f102aa9f9ae77cb879526
John M. Daly <jmdaly@gmail.com>
2016-02-01 14:15:17 -0500

    Installed required dependencies for pdf generation with Sphinx

--- install_sphinx.sh
+++ install_sphinx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This script installs Sphinx, the document creation tool.
-apt-get install -y python-pip texlive
+apt-get install -y python-pip texlive texlive-latex-extra texlive-fonts-recommended
 pip install -U Sphinx

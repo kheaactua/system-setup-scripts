fdd53fae2b8f0ec5a26cd0d8f86b854ff4f8ccdd
John M Daly <jmdaly@gmail.com>
2016-03-19 15:06:30 -0400

    Initial commit of script to download, build, and install rofi. Untested.

---
+++ install_rofi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script downloads, configures, and builds rofi,
+# a window launcher that can be used with i3 instead of
+# demnu
+
+# First, install the build dependencies
+apt-get install -y git make gcc checkinstall autoconf automake pkg-config
+apt-get install -y libxinerama-dev libxft-dev libpango1.0-dev libcairo2-dev libpangocairo-1.0-0 libglib2.0-dev libx11-dev libstartup-notification0-dev 
+
+apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+
+INSTALL_PREFIX=/usr/local
+
+# Get the source:
+if [ ! -d ${INSTALL_PREFIX}/src/rofi ]; then
+  mkdir -p ${INSTALL_PREFIX}/src
+  cd ${INSTALL_PREFIX}/src
+  git clone https://github.com/DaveDavenport/rofi.git 
+fi
+
+ROFI_VERSION=0.15.11
+cd ${INSTALL_PREFIX}/src/rofi
+git checkout ${ROFI_VERSION}
+autoreconf -i
+mkdir build && cd build
+../configure
+make -j && checkinstall -D -y --pkgname rofi --pkgversion ${ROFI_VERSION} --pkggroup x11 make install

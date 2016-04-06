50a7a63b3dcc455e9b5dead147c6798998a4a9ca
John M Daly <jmdaly@gmail.com>
2016-04-06 11:48:50 -0400

    Added script to build rr from source and install it

---
+++ install_rr.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script installs rr, the deterministic debugger.
+
+# First install the build dependencies:
+sudo apt-get -y install ccache cmake make g++-multilib gdb \
+  pkg-config libz-dev realpath python-pexpect manpages-dev git zlib1g-dev \
+  ninja-build checkinstall
+
+INSTALL_PREFIX=/usr/local
+
+# Get the source:
+if [ ! -d ${INSTALL_PREFIX}/src/rr ]; then
+  mkdir -p ${INSTALL_PREFIX}/src
+  git clone https://github.com/mozilla/rr.git ${INSTALL_PREFIX}/src/rr
+fi
+
+RR_VERSION=4.2.0
+cd ${INSTALL_PREFIX}/src/rr
+git fetch --tags
+git checkout ${ROFI_VERSION}
+mkdir -p build && cd build
+cmake ..
+make -j && checkinstall -D -y --pkgname rr --pkgversion ${RR_VERSION} make install
+

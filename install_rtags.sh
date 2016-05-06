e6886bffb038538424ae39d93046c9b15d7da58b
John M Daly <jmdaly@gmail.com>
2016-05-06 10:06:54 -0400

    Modified rtags script to upgrade clang version

--- install_rtags.sh
+++ install_rtags.sh
@@ -4,15 +4,16 @@
 
 # First, check if we have the correct sources for the newest version of
 # clang:
+CLANG_VERSION=3.8
 if [ ! -f /etc/apt/sources.list.d/llvm.list ]; then
   wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|apt-key add -
-  echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main" > /etc/apt/sources.list.d/llvm.list
-  echo "deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main" >> /etc/apt/sources.list.d/llvm.list
+  echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-${CLANG_VERSION} main" > /etc/apt/sources.list.d/llvm.list
+  echo "deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-${CLANG_VERSION} main" >> /etc/apt/sources.list.d/llvm.list
 fi
 
 # install the required build dependencies:
 apt-get update
-apt-get install -y cmake ninja-build clang-3.7 libclang-3.7-dev libclang-common-3.7-dev libclang1-3.7 libllvm3.7 libncurses5-dev libssl-dev git g++
+apt-get install -y cmake ninja-build clang-${CLANG_VERSION} libclang-${CLANG_VERSION}-dev libclang-common-${CLANG_VERSION}-dev libclang1-${CLANG_VERSION} libllvm${CLANG_VERSION} libncurses5-dev libssl-dev git g++
 
 INSTALL_PREFIX=/usr/local
 
@@ -32,5 +33,5 @@ git submodule update
 cd ${INSTALL_PREFIX}/src/rtags/build
 
 # Build and install the source:
-CXX=clang++-3.7 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DLIBCLANG_LLVM_CONFIG_EXECUTABLE=/usr/bin/llvm-config-3.7 .. && ninja install
+CXX=clang++-${CLANG_VERSION} cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DLIBCLANG_LLVM_CONFIG_EXECUTABLE=/usr/bin/llvm-config-${CLANG_VERSION} .. && ninja install
 

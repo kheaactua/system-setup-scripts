3495f6d1aa486da974c6e4da852a251f4d0ef872
Matthew Russell <mrussell@neptec.com>
2016-05-17 15:12:21 -0400

    Added set-alternatives

--- install_neovim.sh
+++ install_neovim.sh
@@ -2,24 +2,34 @@
 
 # This script sets up a PPA and installs
 # neovim.
+function install_neovim() {
 
-# See if neovim is already installed:
-if hash nvim 2>/dev/null; then
-  echo "Neovim already installed"
-  exit 0
-fi
+	# See if neovim is already installed:
+	if hash nvim 2>/dev/null; then
+	  echo "Neovim already installed"
+	  exit 0
+	fi
 
-add-apt-repository -y ppa:neovim-ppa/unstable
-apt-get update
-apt-get install -y neovim
+	add-apt-repository -y ppa:neovim-ppa/unstable
+	apt-get update
+	apt-get install -y neovim
 
-# The following dependencies are required for
-# python support in neovim:
-apt-get install -y python-dev python-pip python3-dev python3-pip
+	# The following dependencies are required for
+	# python support in neovim:
+	apt-get install -y python-dev python-pip python3-dev python3-pip
 
-# This dependency is require for using the system clipboard:
-apt-get install -y xsel
+	# This dependency is require for using the system clipboard:
+	apt-get install -y xsel
 
-# Then install the python modules:
-pip2 install neovim
-pip3 install neovim
+	# Then install the python modules:
+	pip2 install neovim                                                         \
+		&& pip3 install neovim                                                   \
+		&& update-alternatives --install /usr/bin/vi     vi     /usr/bin/nvim 60 \
+		&& update-alternatives --install /usr/bin/vim    vim    /usr/bin/nvim 60 \
+		&& update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60 \
+
+}
+
+install_neovim
+
+# vim: ts=3 sw=3 sts=0 noet :

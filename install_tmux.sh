716013afcc6d737736dcd8449aa8424c6c942e49
Matthew Russell <mrussell@neptec.com>
2016-05-18 15:09:27 -0400

    Refactored to use a function, and added the ability to choose a branch.

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-# This script sets up a PPA to install the latest
+# Set up a PPA to install the latest
 # version of tmux. The one that ships with Ubuntu
 # 14.04 is too old to use plugins with.
+function install_tmux() {
 
-# Build dependencies
-apt-get install -y automake make gcc libevent-dev libncurses5-dev
+	local -r TAG=$1
 
-# This dependency is required for using the system clipboard:
-apt-get install -y xsel
+	# Build dependencies
+	apt-get install -y automake make gcc libevent-dev libncurses5-dev
 
-INSTALL_PREFIX=/usr/local
+	# This dependency is required for using the system clipboard:
+	apt-get install -y xsel
 
-# Get the source:
-if [ ! -d ${INSTALL_PREFIX}/src/tmux ]; then
-  mkdir -p ${INSTALL_PREFIX}/src
-  cd ${INSTALL_PREFIX}/src
-  git clone https://github.com/tmux/tmux.git
-fi
+	local -r INSTALL_PREFIX=/usr/local
 
-cd ${INSTALL_PREFIX}/src/tmux
-git pull
-sh autogen.sh
-./configure && make && make install
+	# Get the source:
+	if [[ ! -d "${INSTALL_PREFIX}/src/tmux" ]]; then
+		mkdir -p "${INSTALL_PREFIX}/src"
+		cd "${INSTALL_PREFIX}/src"
+		git clone https://github.com/tmux/tmux.git
+	fi
+
+	cd "${INSTALL_PREFIX}/src/tmux"
+	git branch ${TAG}
+	git pull
+
+	sh autogen.sh      \
+		&& ./configure  \
+		&& make         \
+		&& make install
+}
+
+install_tmux 2.2
+
+# vim: ts=3 sw=3 sts=0 noet :

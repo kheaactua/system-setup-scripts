4505252e19aa2964cf28405e7eaae6daf32d49b4
John M Daly <jmdaly@gmail.com>
2016-06-09 15:21:40 -0400

    Switch i3blocks to gaps version

--- install_i3blocks.sh
+++ install_i3blocks.sh
@@ -3,17 +3,18 @@
 # This script downloads, builds, and install i3blocks
 
 # install the required build dependencies:
-apt-get update
 apt-get install -y ruby-ronn sysstat acpi
 
+INSTALL_PREFIX=/usr/local
+
 # Get the source:
-if [ ! -f /usr/local/bin/i3blocks ]; then
-  mkdir -p ~/git
-  cd ~/git
-  if [ ! -d ~/git/i3blocks ]; then
-    git clone https://github.com/vivien/i3blocks.git
-  fi
+if [ ! -d ${INSTALL_PREFIX}/src/i3blocks-gaps ]; then
+  mkdir -p ${INSTALL_PREFIX}/src
+  cd ${INSTALL_PREFIX}/src
+  git clone https://github.com/Airblader/i3blocks-gaps.git
 fi
 
-cd ~/git/i3blocks
-make install
+I3BLOCKS_VERSION=1.4
+cd ${INSTALL_PREFIX}/src/i3blocks-gaps
+git pull
+make -j && checkinstall -D -y --pkgname i3blocks-gaps --pkgversion ${I3BLOCKS_VERSION} --pkggroup x11 make install

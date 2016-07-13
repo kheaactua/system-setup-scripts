05afb2943f6274548cb2afe48687c88a4e3b0384
Matthew Russell <mrussell@neptec.com>
2016-07-13 08:55:06 -0400

    Added alias for clang tidy, and fixed the line continuations

--- install_clang.sh
+++ install_clang.sh
@@ -26,10 +26,11 @@ function install_version() {
 	# install the required build dependencies:
 	apt-get update  \
 		&& apt-get install -y clang-${v} libclang-${v}-dev libclang-common-${v}-dev libclang1-${v} libllvm${v} libncurses5-dev libssl-dev \
-		&& update-alternatives --install /usr/bin/clang++         clang++         /usr/bin/clang++-${v}         100
-		&& update-alternatives --install /usr/bin/clang           clang           /usr/bin/clang++-${v}         100
-		&& update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-${v} 100
-		&& update-alternatives --install /usr/bin/lldb-server     lldb-server     /usr/bin/lldb-server-${v}     100
+		&& update-alternatives --install /usr/bin/clang++         clang++         /usr/bin/clang++-${v}         100                       \
+		&& update-alternatives --install /usr/bin/clang           clang           /usr/bin/clang++-${v}         100                       \
+		&& update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-${v} 100                       \
+		&& update-alternatives --install /usr/bin/lldb-server     lldb-server     /usr/bin/lldb-server-${v}     100                       \
+		&& update-alternatives --install /usr/bin/clang-tidy      clang-tidy      /usr/bin/clang-tidy-${v}      100
 }
 
 install_version "${VERSION}"

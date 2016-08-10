16b21760d9b5808fd05c6d642cadba0a73ca6f81
Matthew Russell <Matthew Russell>
2016-08-10 09:01:58 -0400

    Spacing and added some packages.

--- install_development.sh
+++ install_development.sh
@@ -4,26 +4,30 @@
 # I use for software development
 
 apt-get install -y g++ \
-   clang \
-   ccache \
-   distcc \
-   doxygen \
-   graphviz \
-   libpython-dev \
-   cppcheck \
-   cmake \
+   clang            \
+   ccache           \
+   distcc           \
+   doxygen          \
+   graphviz         \
+   libpython-dev    \
+   cppcheck         \
+   cmake            \
    cmake-curses-gui \
-   cmake-qt-gui \
-   make \
-   ninja-build \
-   build-essential \
-   libXt \
-   libxt-dev \
-   libXNVCtrl \
+   cmake-qt-gui     \
+   make             \
+   ninja-build      \
+   build-essential  \
+   libXt            \
+   libxt-dev        \
+   libXNVCtrl       \
    libglu1-mesa-dev \
-   libssl-dev \
-   dh-autoreconf \
-   pkg-config \
+   libssl-dev       \
+   dh-autoreconf    \
+   pkg-config       \
+   libglib2.0-dev   \
+   libbz2-dev       \
+   inputplug        \
+   python-rosdep python-rosinstall-generator python-wstool python-rosinstall \
 
 
 # vim: ts=3 st=3 sts=0 expandtab ft=sh ffs=unix :

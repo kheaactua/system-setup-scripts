0457bd1138d5a893c03e3912e18d9b3c2a48401f
Matthew Russell <matthew.g.russell@gmail.com>
2017-07-23 11:39:25 -0400

    Added llvm-config to the mix. (alternatives)

--- install_clang.sh
+++ install_clang.sh
@@ -82,6 +82,7 @@ function install_version_apt() {
 		&& update-alternatives --install /usr/bin/lldb-server     lldb-server     ${install_path}/lldb-server-${v}     ${priority}  \
 		&& update-alternatives --install /usr/bin/clang-tidy      clang-tidy      ${install_path}/clang-tidy-${v}      ${priority}  \
 		&& update-alternatives --install /usr/bin/clang-format    clang-format    ${install_path}/clang-format-${v}    ${priority}  \
+		&& update-alternatives --install /usr/bin/llvm-config    llvm-config      ${install_path}/llvm-config          ${priority}  \
 
 }
 

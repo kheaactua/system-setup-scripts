60537c9834b548239a9c74f4d9fc2f029899fcb8
Matthew Russell <mrussell@neptec.com>
2017-08-10 14:35:48 -0400

    Updated to 0.7, wrote code to apply patches.

--- install_st.sh
+++ install_st.sh
@@ -1,37 +1,64 @@
 #!/bin/bash -e
 
 declare -r INSTALL_PREFIX=/usr/local
-declare -r ST_VERSION=0.6
+declare -r ST_TAG=0.7
 
 # This script downloads, configures, and builds the suckless
 # terminal
 function install_st() {
 
 	local -r prefix=$1
-	local -r version=$2
-	local -r force=${3:-0}
+	local -r tag=$2
+	local -r gen_new_config=${3:-0}
 
 	# First, install the build dependencies
-	apt-get install -y libxft-dev curl make gcc libxext-dev
+	# apt-get install -y libxft-dev curl make gcc libxext-dev
 
 	# Get the source:
-	if [[ ! -d "${prefix}/src/st-${version}" && "${force}" != "1" ]]; then
-		curl -fLo /tmp/st-${version}.tar.gz http://dl.suckless.org/st/st-0.6.tar.gz
-		mkdir -p ${prefix}/src
-		cd ${prefix}/src && tar xvzf /tmp/st-${version}.tar.gz
+	cd ${prefix}/src
+	if [[ ! -d "st" ]]; then
+		git clone http://git.suckless.org/st/
+	fi
+	cd st
+
+	git fetch
+	git checkout ${tag}
+
+	# Generate or download config
+	if [[ "${gen_new_config}" == 1 ]]; then
+		# Apply patches
+		local -a patches
+
+		# Light Solarized
+		patches+=http://st.suckless.org/patches/solarized/st-solarized-light-0.7.diff
+
+		for p in $patches; do
+			wget $p
+			patch < $(basename $p)
+		done;
+
+		# /Apply Patches
+		#
+
+		# Generate a config file
+		make
+	else
+		# Get my custom config from github:
+		if [[ -e ~/dotfiles/st/config.h ]]; then
+			ln -s ~/dotfiles/st/config.h
+		else
+			curl -fLo config.h https://raw.githubusercontent.com/kheaactua/dotfiles/master/st/config.h
+		fi
 	fi
 
-	# Get my custom config from github:
-	curl -fLo ${prefix}/src/st-${version}/config.h https://raw.githubusercontent.com/kheaactua/dotfiles/master/st/config.h
-	if [[ $? != 0 ]]; then
+	if [[ -e config.h ]]; then
+		make clean install
+	else
 		echo "Could not download config.h" >&2
 		exit -1;
 	fi
-
-	cd ${prefix}/src/st-${version}
-	make clean install
 }
 
-install_st ${INSTALL_PREFIX} ${ST_VERSION}
+install_st ${INSTALL_PREFIX} ${ST_TAG}
 
 # vim: ts=3 sw=3 sts=0 noet ffs=unix :

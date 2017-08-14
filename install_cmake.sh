71be69f563b7972970b774ce367b579818dbb307
Matthew Russell <matthew.g.russell@gmail.com>
2017-08-14 15:02:47 -0400

    Enabling parallel builds

--- install_cmake.sh
+++ install_cmake.sh
@@ -8,7 +8,7 @@ function install_cmake() {
 	local -r INSTALL_PREFIX=${2:-/usr/local}
 
 	# Get the source:
-	if [[ ! -d "${INSTALL_PREFIX}/src/cmake" ]]; then
+	if [[ ! -d "${INSTALL_PREFIX}/src/CMake" ]]; then
 		mkdir -p "${INSTALL_PREFIX}/src"
 		cd "${INSTALL_PREFIX}/src"
 		git clone https://github.com/Kitware/CMake
@@ -19,7 +19,17 @@ function install_cmake() {
 	git checkout ${TAG}
 	git pull
 
-	./bootstrap                      \
+	local flags=""
+	flags="--parallel=4"
+
+	# Is ccache available?
+	ccache_path=$(which ccache 2>/dev/null)
+	if [[ $? == 0 ]]; then
+		flags+=" --enable-ccache"
+	fi
+
+
+	./bootstrap ${flags}             \
 		&& make                       \
 		&& make install
 
@@ -27,13 +37,15 @@ function install_cmake() {
 	if [[ "${ret}" == 0 ]]; then
 		local -r priority=$(expr $(getPriority cmake) + 1)
 
-		update-alternatives --install /usr/bin/cmake cmake ${INSTALL_PREFIX}/bin/cmake ${priority}
+		update-alternatives --install /usr/bin/cmake cmake ${INSTALL_PREFIX}/bin/cmake ${priority}    \
+		&& update-alternatives --install /usr/bin/ctest ctest ${INSTALL_PREFIX}/bin/ctest ${priority} \
+		&& update-alternatives --install /usr/bin/cpack cpack ${INSTALL_PREFIX}/bin/cpack ${priority}
 
 	else
 		echo "CMake build failed"
 	fi
 }
 
-install_cmake 3.9.0
+install_cmake v3.9.1
 
 # vim: ts=3 sw=3 sts=0 noet :

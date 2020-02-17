43a123436a5504db20fa6729f0bcca2d32564cb4
Matthew Russell <matthew.g.russell@gmail.com>
2020-02-17 13:50:05 -0500

    Added an 'install from ppa' for CMake

--- install_cmake.sh
+++ install_cmake.sh
@@ -2,7 +2,7 @@
 
 source getPriority.sh
 
-declare -r version=${1:-v3.10.2}
+declare -r version=${1:-v3.16.4}
 
 if [ "0" == "$(id -u)" ]; then
 	declare -r prefix=${2:-/usr/local}
@@ -10,8 +10,26 @@ else
 	declare -r prefix=${2:-${HOME}}
 fi
 
-function install_cmake() {
+function getIssueName() {
+	local -r issue="$(cat /etc/issue)"
+	if [[ "${issue}" =~ 14.04* ]]; then
+		echo "trusty"
+	elif [[ "${issue}" =~ 16.04* ]]; then
+		echo "xenial"
+	elif [[ "${issue}" =~ 18.04* ]]; then
+		echo "bionic"
+	elif [[ "${issue}" =~ 19.04* ]]; then
+		echo "disco"
+	elif [[ "${issue}" =~ 19.10* ]]; then
+		echo "eoan"
+	else
+		echo "Unknown issue!" >&2
+		exit 1
+	fi
+}
 
+function install_cmake_from_src()
+{
 	local -r TAG=$1
 	local -r INSTALL_PREFIX=${2:-/usr/local}
 
@@ -74,6 +92,29 @@ function install_cmake() {
 	else
 		echo "Not setting alternatives (requires root permission)"
 	fi
+}
+
+function install_cmake_from_ppa() {
+	# https://apt.kitware.com/
+	apt-get update && sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget
+
+	wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
+
+	apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(getIssueName) main"
+	apt-get update
+
+	apt-get install kitware-archive-keyring
+	apt-key --keyring /etc/apt/trusted.gpg del C1F34CDD40CD72DA
+
+	apt install cmake -y
+}
+
+function install_cmake()
+{
+	local -r TAG=$1
+	local -r INSTALL_PREFIX=${2:-/usr/local}
+
+	install_cmake_from_ppa
 }
 
 install_cmake "${version}" "${prefix}"

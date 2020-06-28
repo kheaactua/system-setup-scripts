a657b620c7f4803557a952bed8f8463f74abab54
Matthew Russell <matthew.g.russell@gmail.com>
2020-06-28 08:27:13 -0400

    Install rofi and check

---
+++ install_check.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+declare -r VERSION=0.15.0
+declare -r INSTALL_PREFIX=/usr/local
+declare -r RUN_AS=matt
+
+function install_check()
+{
+  local tag="${1}"
+  local install_prefix=${2:-/usr/local}
+  local run_as="${3:root}"
+
+  # First, install the build dependencies
+  apt install -y \
+    git make gcc checkinstall autoconf automake pkg-config
+
+  # Get the source:
+  if [ ! -d "${install_prefix}/src/check" ]; then
+    mkdir -p "${install_prefix}/src"
+    cd "${INSTALL_PREFIX}/src"
+    git clone --recursive https://github.com/libcheck/check.git
+    chown -R "${run_as}": "${install_prefix}/src/check"
+  fi
+
+  cd "${install_prefix}/src/check"
+  sudo -u "${run_as}" git fetch --tags
+  sudo -u "${run_as}" git checkout "${tag}"
+  sudo -u "${run_as}" autoreconf -i
+  sudo -u "${run_as}" mkdir -p build && cd build
+  sudo -u "${run_as}" ../configure --prefix="${install_prefix}"
+  sudo -u "${run_as}" make \
+    && sudo -u "${run_as}" check \
+    && sudo -u "${run_as}" install \
+    && sudo -u "${run_as}" ldconfig
+}
+
+install_check "${VERSION}" "${INSTALL_PREFIX}" "${RUN_AS}"
+
+# vim: ts=2 sw=2 sts=0 expandtab :
--- install_rofi.sh
+++ install_rofi.sh
@@ -2,11 +2,13 @@
 
 declare -r VERSION=1.5.4
 declare -r INSTALL_PREFIX=/usr/local
+declare -r RUN_AS=matt
 
 function install_rofi()
 {
   local tag="${1}"
   local install_prefix=${2:-/usr/local}
+  local run_as="${3:root}"
 
   # This script downloads, configures, and builds rofi,
   # a window launcher that can be used with i3 instead of
@@ -16,26 +18,29 @@ function install_rofi()
   apt install -y \
     git make gcc checkinstall autoconf automake pkg-config
   apt install -y \
-    libxinerama-dev libxft-dev libpango1.0-dev libcairo2-dev libpangocairo-1.0-0 libglib2.0-dev libx11-dev libstartup-notification0-dev
-
+    libxinerama-dev libxft-dev libpango1.0-dev libcairo2-dev \
+    libpangocairo-1.0-0 libglib2.0-dev libx11-dev libstartup-notification0-dev \
+    bison flex libxkbcommon-x11-0 libxkbcommon-dev libxkbcommon-x11-dev \
+    librsvg2-dev libev-dev libpam0g-dev libxcb-dpms0-dev libxkbfile-dev \
 
   # Get the source:
   if [ ! -d "${install_prefix}/src/rofi" ]; then
     mkdir -p "${install_prefix}/src"
     cd "${INSTALL_PREFIX}/src"
-    git clone https://github.com/DaveDavenport/rofi.git
+    git clone --recursive https://github.com/DaveDavenport/rofi.git
+    chown -R "${run_as}": "${install_prefix}/src/rofi"
   fi
 
-  ROFI_VERSION=1.5.4
   cd "${install_prefix}/src/rofi"
-  git fetch --tags
-  git checkout "${tag}"
-  autoreconf -i
-  mkdir -p build && cd build
-  ../configure
-  make -j && checkinstall -D -y --pkgname rofi --pkgversion "${tag}" --pkggroup x11 make install
+  sudo -u "${run_as}" git fetch --tags
+  sudo -u "${run_as}" git checkout "${tag}"
+  sudo -u "${run_as}" autoreconf -i
+  sudo -u "${run_as}" mkdir -p build && cd build
+  sudo -u "${run_as}" ../configure --prefix="${install_prefix}"
+  sudo -u "${run_as}" make -j \
+    && checkinstall -D -y --pkgname rofi --pkgversion "${tag}" --pkggroup x11 make install
 }
 
-install_rofi "${VERSION}" "${INSTALL_PREFIX}"
+install_rofi "${VERSION}" "${INSTALL_PREFIX}" "${RUN_AS}"
 
 # vim: ts=2 sw=2 sts=0 expandtab :

17097961e6fc574ad61e97567f8e1ebe96e7fdc3
Matthew Russell <matthew.g.russell@gmail.com>
2021-06-03 09:50:41 -0400

    Added nvim

--- devel/conanfile.py
+++ devel/conanfile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from conans import ConanFile, tools
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 
 import os
 
@@ -8,11 +9,13 @@ class DevelConan(ConanFile):
     settings = 'os'
     description = 'Install Development Tools'
     name = 'devel'
-    version = '0.0.1'
+    version = '0.0.2'
     generators = 'virtualenv'
 
     build_requires = (
-        'ccache/[>=4.2.1]'
+        'ccache/[>=4.2.1]@matt/testing',
         'cmake/[>=3.20.2]',
         'ninja/1.10.2',
+        'gtest/1.10.0',
+        'nvim/0.5.0-dev-056c464e@matt/nightly',
     )

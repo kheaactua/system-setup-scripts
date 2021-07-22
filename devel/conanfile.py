a1c260fe77b238b167a293cae08b40f4049b8194
Matthew Russell <matthew.g.russell@gmail.com>
2021-07-22 12:24:29 -0400

    Updated versions

--- devel/conanfile.py
+++ devel/conanfile.py
@@ -9,13 +9,13 @@ class DevelConan(ConanFile):
     settings = 'os'
     description = 'Install Development Tools'
     name = 'devel'
-    version = '0.0.2'
+    version = '0.0.3'
     generators = 'virtualenv'
 
     build_requires = (
         'ccache/[>=4.2.1]',
-        'cmake/[>=3.20.2]',
+        'cmake/[>=3.20.4]',
         'ninja/1.10.2',
         'gtest/1.10.0',
-        'nvim/0.5.0-dev-24e0c16',
+        'nvim/0.5.0-dev-6f48c018',
     )

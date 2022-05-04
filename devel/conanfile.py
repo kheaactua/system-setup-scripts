ebe8ef3d9e6af6682fea67c2c54e7ed5050d4000
Matthew Russell <matthew.g.russell@gmail.com>
2022-05-04 09:32:38 -0400

    Updating ccache and cmake

--- devel/conanfile.py
+++ devel/conanfile.py
@@ -9,12 +9,12 @@ class DevelConan(ConanFile):
     settings = 'os'
     description = 'Install Development Tools'
     name = 'devel'
-    version = '0.0.9'
+    version = '0.0.10'
     generators = 'virtualenv'
 
     build_requires = (
-        'ccache/[>=4.2.1]',
-        'cmake/[>=3.22]',
+        'ccache/[>=4.6]',
+        'cmake/[>=3.23]',
         'ninja/1.10.2',
         'gtest/1.11.0',
         'nvim/v0.7.0-333ba65'
